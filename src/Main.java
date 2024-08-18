fed514af1afa6ed689ef55b2bab9cc3f7465f43c
Abi <Abishekregme@gmail.com>
2024-08-17 23:37:28 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("enter your age \n");
+        int age = 25; //Person age (can be changed )
+        if (age >= 21) //Condition
+            System.out.println("You get a paper wrist band ");
+    }
+}
